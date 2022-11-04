5d80f5501fe8fae91312baeadfb9d77ea212f875
miguelariasdev <miguelariasdev@gmail.com>
2022-11-04 04:07:42 +0100

    first commit

---
+++ 01-Introduccion/js/app.js
@@ -0,0 +1,10 @@
+// Comentar codigo --> alert("Hola Mundo")
+/* Toma el nombre y lo muestra por pantalla*/
+const nombre = prompt ("Cual es tu nombre?");
+document.querySelector(".contenido").innerHTML = `${nombre} esta aprendiendo javascript moderno`
+
+function hola () {
+    console.log("Ok")
+    console.log("Hola")
+    console.log("Mundo")
+}
